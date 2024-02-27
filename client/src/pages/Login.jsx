9f9debf6aec41782e03470a30bde95a4c522ce18
chandan9pandey <chandan9pandey@gmail.com>
2024-02-27 18:39:29 +0530

    + login page validation toasts

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
 	const [formData, setFormData] = useState({
@@ -14,6 +16,17 @@ const Login = () => {
 
 	const baseUrl = import.meta.env.VITE_BASE_URL; // Server Url
 
+	const toastProperties = {
+		position: "top-right",
+		autoClose: 5000,
+		hideProgressBar: false,
+		closeOnClick: true,
+		pauseOnHover: true,
+		draggable: true,
+		progress: undefined,
+		theme: "light",
+	};
+
 	const handleLogin = async (e) => {
 		e.preventDefault();
 		const data = {
@@ -23,13 +36,20 @@ const Login = () => {
 
 		const validationErrors = {};
 		if (!formData?.email.trim()) {
+			toast.error("Please enter your email address", toastProperties);
 			validationErrors.email = "Please enter your email address";
 		} else if (!/^\S+@\S+\.\S+$/.test(formData?.email)) {
+			toast.error("Email address is invalid", toastProperties);
 			validationErrors.email = "Email address is invalid";
 		}
 		if (!formData?.password.trim()) {
+			toast.error("Please enter your password", toastProperties);
 			validationErrors.password = "Please enter your password";
 		} else if (formData?.password.length < 6) {
+			toast.error(
+				"Password should be at least 6 characters long",
+				toastProperties
+			);
 			validationErrors.password =
 				"Password should be at least 6 characters long";
 		}
@@ -82,9 +102,9 @@ const Login = () => {
 						onChange={handleInputChange}
 						name="email"
 					/>
-					{errors.email && (
+					{/* {errors.email && (
 						<span className="text-red-500 text-lg">{errors.email}</span>
-					)}
+					)} */}
 					<input
 						type="password"
 						placeholder="Password"
@@ -92,9 +112,9 @@ const Login = () => {
 						onChange={handleInputChange}
 						name="password"
 					/>
-					{errors.password && (
+					{/* {errors.password && (
 						<span className="text-red-500 text-lg">{errors.password}</span>
-					)}
+					)} */}
 					<button
 						className="bg-primary p-2 w-full text-white text-lg rounded-2xl my-3"
 						onClick={(e) => handleLogin(e)}
